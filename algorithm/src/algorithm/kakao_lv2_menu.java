7f3bc735624639010c61dcb9e23dc948852d1f0a
woo <woo@localhost>
2022-03-06 15:25:18 +0900

    lv2 menu renew complete

--- algorithm/src/algorithm/kakao_lv2_menu.java
+++ algorithm/src/algorithm/kakao_lv2_menu.java
@@ -2,6 +2,7 @@ package algorithm;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -10,9 +11,10 @@ public class kakao_lv2_menu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String []arr = {"XYZ", "XWY", "WXA"};
-		int []course = {2,3,5};
+		String []arr = {"ABCD", "ABCD", "ABCD"};
+		int []course = {2,3,4};
 		System.out.println("##"+solution(arr, course));
+		//'AB', 'ABC', 'ABCD', 'ABD', 'AC', 'ACD', 'AD', 'BC', 'BCD', 'BD', 'CD'
 	}
     private static HashMap<String, Integer> comMap = new HashMap<String, Integer>();
     private static StringBuilder sb = new StringBuilder();
@@ -20,63 +22,68 @@ public class kakao_lv2_menu {
         for (int i = 0; i < orders.length; i++) {
             char[] charArr = orders[i].toCharArray();
             boolean[] visited = new boolean[charArr.length];
-
+            Arrays.sort(charArr);
             for (int j = 0; j < course.length; j++) {
 
                 if(charArr.length>=course[j]) {
-                    dfs(charArr, visited, 0, charArr.length, course[j], course[j]);
+                    //dfs(charArr, visited, 0, charArr.length, course[j], course[j]);
+                	combination(charArr, visited, 0  ,charArr.length, course[j]);
                 }
             }
         }
     	List<String> list = new  ArrayList<String>();
-    	String temp ="";
     	int maxcount=0;
     
     	for(int i = 0 ; i < course.length; i ++) {
-    		temp = "";
     		maxcount = 0;
         	Iterator<String > it = comMap.keySet().iterator();
+        	Iterator<String > it2 = comMap.keySet().iterator();
+
     		while(it.hasNext()) {
     			String key = it.next();
-	    		
-
-    			//	System.out.println("##"+key+"##"+key.length());
-    			if(comMap.containsKey(key)&& comMap.get(key) > 1  &&  key.length() == course[i] ) {
-    				if(maxcount ==0 ) {
+    			if(comMap.get(key) > 1  &&  key.length() == course[i] ) {
+    				if(maxcount == 0 ) {
     	    			maxcount  = comMap.get(key);
-        				temp = key;
-       					//System.out.println("##"+course[i]+"##"+temp);
         			}else {
-   	    				if(maxcount == comMap.get(key)) {
-   	    					temp =temp+ ","+key;
-   	    				}else if(maxcount < comMap.get(key)) {
+   	    				 if(maxcount < comMap.get(key)) {
    	    					maxcount  = comMap.get(key);
-   	    					temp = key;
     	    			}
     	    		}
     	    	}
     		}
-    		if(!temp.equals("")) {
-	    		if(temp.contains(",")) {
-	    			String [] temparr = temp.split(",");
-	    			for(String temp2 : temparr) {
-	    				list.add(temp2);
-	    			}
-	    		}else {
-	    			list.add(temp);
-    		}}
+    		while(it2.hasNext()) {
+    			String key = it2.next();
+    			if(comMap.get(key) > 1 && key.length() == course[i] && comMap.get(key) == maxcount) {
+    				list.add(key);
+    			}
+    		}
     	}
-    	String [] answer = new String[list.size()];
 
-    	for(int i = 0 ; i < list.size(); i ++) {
-    		answer[i] = list.get(i);
-    	}
-    	Arrays.sort(answer);
+        String[] answer = list.toArray(new String[list.size()]);
+        Collections.sort(list);
+
+
     	for(String a  : answer) {
     		System.out.println("#"+a);
     	}
+    	
+    	// 'AB', 'ABC', 'ABCD', 'ABD', 'AC', 'ACD', 'AD', 'BC', 'BCD', 'BD', 'CD'
     	return answer;
     }
+//    private static void combination(char[] charArr, boolean[] visited, int start, int n, int r) {
+//        if (r == 0) {
+//            push(charArr, visited, n);
+//            return;
+//        }
+//
+//        for (int i = start; i < n; i++) {
+//            visited[i] = true;
+//            combination(charArr, visited, i + 1, n, r - 1);
+//            visited[i] = false;
+//        }
+//    }
+    
+    
     public static void dfs(char [] obj, boolean [] visited, int start, int end, int course ,int course2) {
     	if(course == 0 ) {
     		push(obj , visited, end, course2);
@@ -85,29 +92,26 @@ public class kakao_lv2_menu {
     	for(int i = start ; i < end; i ++) {
     		
     		visited[i] = true;
-    		dfs(obj, visited, start +1 , end , course -1, course2);
+    		dfs(obj, visited, i +1 , end , course -1, course2);
     		visited[i] = false;
     		
     	}
     }
-    public static void push(char [] obj, boolean [] visited , int end, int course) {
+     public static void push(char [] obj, boolean [] visited , int end, int course) {
+
+   // public static void push(char [] obj, boolean [] visited , int end) {
     	sb.setLength(0);
     	
     	for(int i = 0 ; i < end ; i ++) {
     		if(visited[i]) {
-    			//System.out.print(obj[i] +"+");
     			sb.append(obj[i]);
     		}
     	}
-		//System.out.println("//"+sb.toString());
 
     	char [] temp = sb.toString().toCharArray();
     	Arrays.sort(temp);
-    	//Arrays.sort(temp);
-    	if(temp.length == course) {
     		String temp2 = new String(temp);
     		comMap.put(temp2, comMap.getOrDefault(temp2, 0) +1);
-    	}
     }
     
 //	public static String[] solution(String[] orders, int[] course) {
